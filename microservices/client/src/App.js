fdb825a2afce86b6459714046dd4f9c8844d1784
alex-levin <alexlevinb@gmail.com>
2020-07-18 14:20:25 -0400

    update

--- microservices/client/src/App.js
+++ microservices/client/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
 import Main from './components/Main/Main';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#757ce8',
+            main: '#009688',
+            dark: '#002884',
+            contrastText: '#fff'
+        },
+        secondary: {
+            light: '#ff7961',
+            main: '#f44336',
+            dark: '#ba000d',
+            contrastText: '#000'
+        }
+    }
+});
 
 function App() {
     return (
         <div>
-            <Main />
+            <MuiThemeProvider theme={theme}>
+                <Main />
+            </MuiThemeProvider>
         </div>
     );
 }
