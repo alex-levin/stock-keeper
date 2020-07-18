872e7d4d09c79c4e0c7550cac41e0e3e69df9574
alex-levin <alexlevinb@gmail.com>
2020-07-18 13:58:33 -0400

    update

--- microservices/client/src/components/StockTable/StockTable.js
+++ microservices/client/src/components/StockTable/StockTable.js
@@ -16,20 +16,20 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const columns = [
     { id: 'symbol', label: 'Symbol', minWidth: 170 },
-    { id: 'open', label: 'Open', minWidth: 100 },
+    { id: 'open', label: 'Open', minWidth: 170 },
     {
         id: 'high',
         label: 'High',
         minWidth: 170,
         align: 'right',
-        format: (value) => value.toLocaleString('en-US')
+        format: (value) => value.toFixed(2)
     },
     {
         id: 'low',
         label: 'Low',
         minWidth: 170,
         align: 'right',
-        format: (value) => value.toLocaleString('en-US')
+        format: (value) => value.toFixed(2)
     },
     {
         id: 'close',
@@ -47,35 +47,34 @@ const columns = [
     }
 ];
 
+const defaultColDef = {
+    sortable: true,
+    resizable: true
+};
+
 const columnDefs = [
     {
         headerName: 'Symbol',
-        resizable: true,
         field: 'symbol'
     },
     {
         headerName: 'Open',
-        resizable: true,
         field: 'open'
     },
     {
         headerName: 'High',
-        resizable: true,
         field: 'high'
     },
     {
         headerName: 'Low',
-        resizable: true,
         field: 'low'
     },
     {
         headerName: 'Close',
-        resizable: true,
         field: 'close'
     },
     {
         headerName: 'Volume',
-        resizable: true,
         field: 'volume'
     }
 ];
@@ -90,12 +89,31 @@ const rows = [
     createData('MSFT', 208.83, 208.84, 208.85, 208.86, 29732453),
     createData('GOOG', 1480.06, 1506.45, 1503.45, 1501.41, 1518133),
     createData('IBM', 121.25, 121.45, 122.54, 135.21, 3110345),
-    createData('TSLA', 1276.69, 1377.79, 1266.04, 1371.58, 19561403)
+    createData('TSLA', 1276.69, 1377.79, 1266.04, 1371.58, 19561403),
+    createData('MRNA', 94.85, 95.15, 92.3, 94.1, 97123456),
+    createData('GILD', 77.51, 78.2, 76.1, 77.23, 6123456),
+    createData('SAM', 630.78, 630.23, 631.45, 632.45, 89123456),
+    createData('UA', 9.18, 9.23, 9.14, 9.12, 3123456),
+    createData('NTFLX', 527.69, 528.32, 530.16, 527.13, 24123456),
+    createData('AMZN', 2999.69, 2998.79, 2997.04, 2998.58, 4123456),
+    createData('DD', 149.51, 149.6, 147.02, 148.47, 800965),
+    createData('JNJ', 34.95, 34.99, 34.38, 34.51, 25443888),
+    createData('VOO', 208.83, 208.84, 208.85, 208.86, 29732453),
+    createData('MCD', 1480.06, 1506.45, 1503.45, 1501.41, 1518133),
+    createData('F', 121.25, 121.45, 122.54, 135.21, 3110345),
+    createData('CAR', 1276.69, 1377.79, 1266.04, 1371.58, 19561403),
+    createData('UT', 94.85, 95.15, 92.3, 94.1, 97123456),
+    createData('BO', 77.51, 78.2, 76.1, 77.23, 6123456),
+    createData('FB', 630.78, 630.23, 631.45, 632.45, 89123456),
+    createData('AAPL', 9.18, 9.23, 9.14, 9.12, 3123456),
+    createData('APA', 527.69, 528.32, 530.16, 527.13, 24123456),
+    createData('KO', 2999.69, 2998.79, 2997.04, 2998.58, 4123456)
 ];
 
 const useStyles = makeStyles({
     root: {
-        width: '90%',
+        width: '90vw',
+        height: '70vh',
         marginTop: 50,
         margin: 'auto',
         marginBottom: 50
@@ -105,7 +123,7 @@ const useStyles = makeStyles({
     }
 });
 
-export default function StickyHeadTable() {
+export default function StockTable() {
     const [redirect, setRedirect] = useState(false);
     const [selectedSymbol, setSelectedSymbol] = useState(null);
 
@@ -151,86 +169,12 @@ export default function StickyHeadTable() {
 
     if (!redirect) {
         return (
-            <Paper className={classes.root}>
-                {/*
-                <TableContainer className={classes.container}>
-                    <Table stickyHeader aria-label="sticky table">
-                        <TableHead>
-                            <TableRow>
-                                {columns.map((column) => (
-                                    <TableCell
-                                        key={column.id}
-                                        align={column.align}
-                                        style={{
-                                            minWidth: column.minWidth
-                                        }}
-                                    >
-                                        {column.label}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows
-                                .slice(
-                                    page * rowsPerPage,
-                                    page * rowsPerPage + rowsPerPage
-                                )
-                                .map((row) => {
-                                    return (
-                                        <TableRow
-                                            hover
-                                            role="checkbox"
-                                            tabIndex={-1}
-                                            key={row.symbol}
-                                            onClick={() => {
-                                                setSelectedSymbol(row.symbol);
-                                                setRedirect(true);
-                                            }}
-                                        >
-                                            {columns.map((column) => {
-                                                const value = row[column.id];
-                                                return (
-                                                    <TableCell
-                                                        key={column.id}
-                                                        align={column.align}
-                                                    >
-                                                        {column.format &&
-                                                        typeof value ===
-                                                            'number'
-                                                            ? column.format(
-                                                                  value
-                                                              )
-                                                            : value}
-                                                    </TableCell>
-                                                );
-                                            })}
-                                        </TableRow>
-                                    );
-                                })}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[10, 25, 100]}
-                    component="div"
-                    count={rows.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onChangePage={handleChangePage}
-                    onChangeRowsPerPage={handleChangeRowsPerPage}
-                />
-                
-                */}
-
-                <div
-                    className="ag-theme-alpine"
-                    style={{
-                        height: '400px',
-                        width: '100%'
-                    }}
-                >
+            <div className="ag-theme-alpine">
+                <div className={classes.root}>
                     <AgGridReact
+                        pagination={true}
+                        paginationPageSize={10}
+                        defaultColDef={defaultColDef}
                         onGridReady={onGridReady}
                         rowSelection="single"
                         columnDefs={columnDefs}
@@ -238,7 +182,7 @@ export default function StickyHeadTable() {
                         rowData={rows}
                     ></AgGridReact>
                 </div>
-            </Paper>
+            </div>
         );
     } else {
         return (
