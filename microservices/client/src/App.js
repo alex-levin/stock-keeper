3c60db874b4b63e2a3e821eaa369278abc88acc9
alex-levin <alexlevinb@gmail.com>
2020-07-18 14:52:32 -0400

    update

--- microservices/client/src/App.js
+++ microservices/client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
 import Main from './components/Main/Main';
 
 const theme = createMuiTheme({
